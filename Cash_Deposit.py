96a39d482846835eac62eac30bf8ab4d912d62a8
odiezeani1-ops <odiezeani1@gmail.com>
2026-01-11 16:03:34 +0100

    Refactor deposit logic into a function

--- Cash_Deposit.py
+++ Cash_Deposit.py
@@ -1,8 +1,9 @@
-balance = 5000 
-deposit = int(input("Enter amount to deposit: "))
-if deposit > 0:
-     balance = balance + deposit 
-      print("Deposit successful")
-      print("Your new balance is:", balance)
-else:
-      print("Invlid deposit amount")
+def deposit(balance):
+ balance = 5000 
+ deposit = int(input("Enter amount to deposit: "))
+ if deposit > 0:
+      balance = balance + deposit 
+       print("Deposit successful")
+       print("Your new balance is:", balance)
+ else:
+       print("Invlid deposit amount")
