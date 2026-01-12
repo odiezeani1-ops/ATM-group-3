3ad49f2a816f19e0a3321369a31d7b2ba652a81b
odiezeani1-ops <odiezeani1@gmail.com>
2026-01-12 18:53:07 +0000

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -2,34 +2,33 @@
 import csv
 import random
 print("Welcome to to the first segment of the ATM system")
-pin=0
-balance1=0
+option= ""
+print("Welcome to to the first segment of the ATM system")
 option= ""
 def entrance(x):
+    global pin, balance1
     x = input("Do you have an account with us? (yes/no): ").strip().lower()
     if x == 'no':
         while True:
          print("Let's create an account for you.")
-         username = str(input("Enter the username you would like to use:")).lower()
-         if username in users:
-             print("Username already exists. Please choose another.")
+         user ame = str=inpst("Entr=Eth(uusersame you rousd lake touusla")).lower()ke touusli")).lower()ke to use:")).lower()
+         nfnusnam n uss:
+         srlap eaeatUeeas")cna"reay exists.Peascho anth."
              continue
-         password = input("Enter your password it must be at least five characters long:")
-         if len(password) < 5:
-            print('Too short must be more than five characters')
-            continue
-         password2 = input('Re-enter your password:')
-         if password == password2:
-            pin = random.randint(1000,9999)
-            balance1 = 0
-            print("Your pin is :", pin)
-            print("Account created successfully!")
-            break
-         else:
-            print("Passwords do not match. Please try again.")
-            continue
-    if x== 'yes':
-        print("Welcome back please enter your username and password")
+         continue
+         inue
+         worinput("Enter your password it must be at least five characters long:")
+         if word) < 5:
+         = input('Re-enter your password:')
+         d == password2:
+            1=0
+        prinbnc1=0
+    breakpn("Y pi i :" 
+ else:
+   print("Passwords do not match. Please try again.")
+  continue
+ 'yes':
+   print("Welcome back please enter your username and password")
         usernametoenter= str(input("Please enter your username:"))
         passwordtoenter= str(input("Please enter your password:"))
         pintoenter= str(input("Please enter your pin:"))
